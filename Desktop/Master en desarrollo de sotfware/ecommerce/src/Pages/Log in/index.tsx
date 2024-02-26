f0453f8bdfaa162812c04a9ad287050db58423c6
unknown <josemari94@gmail.com>
2024-02-26 17:01:56 +0100

    Improve logic of login

--- Desktop/Master en desarrollo de sotfware/ecommerce/src/Pages/Log in/index.tsx
+++ Desktop/Master en desarrollo de sotfware/ecommerce/src/Pages/Log in/index.tsx
@@ -225,7 +225,7 @@ const Login: React.FC<Props> = ({ onLoginSuccess }) => {
 
 export default Login;*/
 
-import React, { useEffect, useState } from 'react';
+/*import React, { useEffect, useState } from 'react';
 import './login.css'
 import { useNavigate } from 'react-router-dom';
 import { useAuthDispatch } from '../../Context/AuthContext';
@@ -259,6 +259,9 @@ async function loginUser(email: string, password: string, users: User[], onLogin
     alert('Credenciales incorrectas. Inténtalo de nuevo.');
   }
 }
+
+
+
 
 const Login: React.FC<Props> = ({ onLoginSuccess }) => {
   const [email, setEmail] = useState('');
@@ -315,6 +318,115 @@ const Login: React.FC<Props> = ({ onLoginSuccess }) => {
                 required
               />
             </div>
+            <button type="submit">LOG IN</button>
+          </form>
+        ) : (
+          <button onClick={handleGoClick}>GO!</button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Login;*/
+
+import React, { useEffect, useState } from 'react';
+import './login.css'
+import { useNavigate } from 'react-router-dom';
+import { useAuthDispatch } from '../../Context/AuthContext';
+
+type User = {
+  email: string;
+  password: string;
+};
+
+type Props = {
+  onLoginSuccess: () => void;
+};
+
+async function getApi() {
+  try {
+    const data = await fetch('src/assets/Data/users.json');
+    const JSONdata: User[] = await data.json();
+    return JSONdata;
+  } catch (error) {
+    throw new Error('Error fetching user data');
+  }
+}
+
+const Login: React.FC<Props> = ({ onLoginSuccess }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
+  const [loginError, setLoginError] = useState(false);
+  const [errorField, setErrorField] = useState<string | null>(null);
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const dispatch = useAuthDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getApi()
+      .then((data) => setUsers(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleGoClick = () => {
+    setShowLoginForm(true);
+  };
+
+  const handleLoginClick = async (event: React.FormEvent) => {
+    event.preventDefault(); // Prevent page reload on form submission
+    const user = users.find((user) => user.email === email && user.password === password);
+    if (user) {
+      dispatch({ type: 'LOGIN' });
+      navigate('/home');
+      onLoginSuccess();
+    } else {
+      // Determine which field caused the login error
+      if (!email || !password) {
+        setErrorField(null); // Clear error field if fields are empty
+      } else if (!email.includes('@')) {
+        setErrorField('email');
+      } else {
+        setErrorField('password');
+      }
+      setLoginError(true);
+    }
+  };
+
+  return (
+    <>
+      <h1 className='titleLogin'>Horizon Watches</h1>
+      <img className='imgLogin' src="src/assets/hands-1866619_1280.webp" alt="" />
+      <div className='containerLogin'>
+        {showLoginForm ? (
+          <form onSubmit={handleLoginClick}>
+            <div className='inputUser'>
+              <input 
+                id="email"
+                type="email" 
+                name="inputEmail"
+                className={`inputLogin ${errorField === 'email' ? 'input-error' : ''}`}
+                autoComplete='off'
+                placeholder='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className='inputPassword'>
+              <input 
+                id="pass"
+                type="password"
+                name="inputPass" 
+                className={`inputLogin ${errorField === 'password' ? 'input-error' : ''}`}
+                placeholder='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            {loginError && <p className="error-message">Incorrect email or password</p>}
             <button type="submit">LOG IN</button>
           </form>
         ) : (
