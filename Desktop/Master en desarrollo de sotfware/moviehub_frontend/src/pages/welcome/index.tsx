e6efcb38de58e462c56924012985303c4b58a615
unknown <josemari94@gmail.com>
2024-05-09 20:24:33 +0200

    connected frontend and backend

--- Desktop/Master en desarrollo de sotfware/moviehub_frontend/src/pages/welcome/index.tsx
+++ Desktop/Master en desarrollo de sotfware/moviehub_frontend/src/pages/welcome/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react"
 
-type Props = {}
+type Props = {
+};
 
 export function Welcome({}: Props) {
+  const { loginWithRedirect } = useAuth0()
+  
+
   return (
-    <div>index</div>
+    <>
+    <button onClick={()=>loginWithRedirect()}>
+            LOGIN
+          </button>
+
+    </>
   )
+
 }
+
+
--- Desktop/Master en desarrollo de sotfware/moviehub_frontend/src/routes/index.tsx
+++ Desktop/Master en desarrollo de sotfware/moviehub_frontend/src/routes/index.tsx
@@ -1,14 +1,18 @@
-import Routes 
-
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Welcome } from "../pages/welcome";
-
+import { Home } from '../pages/home';
 
 const AppRoutes = () => {
-    return (
-      <Routes>
-        <Welcome/>
-      </Routes>
-    );
-  };
-  
-  export default AppRoutes;
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" Component={Welcome} />
+      <Route path="home" Component={Home} />
+      {/* Otras rutas aquí */}
+    </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
+
