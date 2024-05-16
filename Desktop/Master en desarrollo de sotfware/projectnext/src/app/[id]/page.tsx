48861b04282f9abfc803a06cb6b1d0fff217d70f
unknown <josemari94@gmail.com>
2024-05-16 20:51:26 +0200

    id

--- Desktop/Master en desarrollo de sotfware/projectnext/src/app/[id]/page.tsx
+++ Desktop/Master en desarrollo de sotfware/projectnext/src/app/[id]/page.tsx
@@ -5,21 +5,24 @@ type Props = {
     params: {id: string} 
 } 
 
+
 export default async function OneMovie ({params}: Props) {
 
-//     const id = params?.id;
-//   const movies: Movie = await getMovie(id);
+    const id = params?.id;
+    const movies: Movie = await getMovie(id);
+    
+    console.log("Movie:", movies);
 
   return (
     <>
-      {/* <div>
+      <div>
         <h1>{movies.name}</h1>
         <img src={movies.image} alt={movies.name}/>
         <div>
           <h3>Sinopsis</h3>
           <p>{movies.sinopsis}</p>
         </div>
-      </div> */}
+      </div>
       <h1>soy una pelicula
       </h1>
     </>
