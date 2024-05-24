149be18f785f1ff8f9a64d323d3193d2d7266b5a
unknown <josemari94@gmail.com>
2024-05-24 12:10:58 +0200

    readme

--- Desktop/Master en desarrollo de sotfware/projectnext/src/app/(pages)/movies/new/action.ts
+++ Desktop/Master en desarrollo de sotfware/projectnext/src/app/(pages)/movies/new/action.ts
@@ -1,33 +1,33 @@
-// // src/app/(pages)/new-movie/actions.ts
-// // src/app/(pages)/new/actions.ts
-// 'use server';
+// src/app/(pages)/new-movie/actions.ts
+// src/app/(pages)/new/actions.ts
+'use server';
 
-// import { revalidatePath } from 'next/cache';
+import { revalidatePath } from 'next/cache';
 
-// interface Movie {
-//   name: string;
-//   image: string;
-//   score: number;
-//   sinopsis: string;
-// }
+interface Movie {
+  name: string;
+  image: string;
+  score: number;
+  sinopsis: string;
+}
 
-// const URL: string = process.env.URL|| "ERROR";
+const URL: string = process.env.URL|| "ERROR";
 
-// export async function createMovie(movie: Movie, userId: string) {
-//   const response = await fetch(URL + "/movie" + userId, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(movie, userId),
-//   });
+export async function createMovie(movie: Movie, userId: string) {
+  const response = await fetch(URL + "/movie" + userId, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(movie, userId),
+  });
 
-//   if (!response.ok) {
-//     throw new Error("Failed to create movie");
-//   }
+  if (!response.ok) {
+    throw new Error("Failed to create movie");
+  }
 
-//   revalidatePath('/movie');
-// }
+  revalidatePath('/movie');
+}
 
 
 
